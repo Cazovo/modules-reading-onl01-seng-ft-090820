acefc0debcc18936d13694cb2022e9d6d4df19bf
conner fortner <fortner.conner@yahoo.com>
2020-09-27 22:03:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,14 +1,15 @@
+  
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 
 class Kid
-  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+
+  attr_accessor :name 
   
-  include Dance
-  include FancyDance
-  attr_accessor :name
- 
   def initialize(name)
     @name = name
   end
